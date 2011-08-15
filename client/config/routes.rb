83074e7308d242e8fe4b1fccc69f9d77aec94206
dom1nga <beloved.dom1nga@gmail.com>
2011-08-15 16:27:51 +0300

    generated user models

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -1,4 +1,6 @@
 Client::Application.routes.draw do
+  devise_for :users
+
   root :to => "pages#home"
 
   # The priority is based upon order of creation:
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Server::Application.routes.draw do
+  devise_for :users
+
   root :to => "pages#home"
 
   # The priority is based upon order of creation:
