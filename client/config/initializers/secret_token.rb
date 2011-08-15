9f6aade9c8aada4ad8600e6387ab1a180bcff370
dom1nga <beloved.dom1nga@gmail.com>
2011-08-15 15:08:32 +0300

    first commit

---
+++ client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Client::Application.config.secret_token = 'f520d9852ce9a80da31faab09db1841ef0f80f926282a036e7b85c66942894276e8c179bb026608a5b5f973e793de80ef2d62d645b199e3d9709cd62d0340676'
---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = 'e7d6c655eee74b383b7f94ff508ff7d8aeb3d4596dc2eb9ef46a32980b8a6463fc587daa5742880da6a1199632e01262de615605f52421f4cd3a6aa4365b4109'
