c411047957aa03864a43cc0b99e56b2dcfa41666
dom1nga <beloved.dom1nga@gmail.com>
2011-08-28 23:48:17 +0300

    login without asking what's going on

--- server/app/controllers/oauth_controller.rb
+++ server/app/controllers/oauth_controller.rb
@@ -2,6 +2,22 @@ require 'oauth/controllers/provider_controller'
 class OauthController < ApplicationController
   include OAuth::Controllers::ProviderController
 
+  def authorize
+    if params[:oauth_token]
+      @token = ::RequestToken.find_by_token! params[:oauth_token]
+      @token.authorize!(current_user)
+      @redirect_url = URI.parse(@token.oob? ? @token.client_application.callback_url : @token.callback_url)
+      @redirect_url.query = @redirect_url.query.blank? ?
+                              "oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}" :
+      @redirect_url.query + "&oauth_token=#{@token.token}&oauth_verifier=#{@token.verifier}"
+      redirect_to @redirect_url.to_s
+    elsif ["code","token"].include?(params[:response_type]) # pick flow
+      send "oauth2_authorize_#{params[:response_type]}"
+    else
+      render :status=>404, :text=>"No token provided"
+    end
+  end
+
   protected
   # Override this to match your authorization page form
   # It currently expects a checkbox called authorize
